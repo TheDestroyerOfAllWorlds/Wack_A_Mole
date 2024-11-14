997700f94aeed1c0f31448c15ccdd88e1c51f184
DestroyerOfWorlds <131493709+TheDestroyerOfAllWorlds@users.noreply.github.com>
2024-11-14 13:27:42 -0500

    Update whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -1,25 +1,33 @@
-import pygame
+import pygame, random
 
+pygame.init()
 
-def main():
-    try:
-        pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
-        mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
-        clock = pygame.time.Clock()
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            screen.fill("light green")
-            pygame.display.flip()
-            clock.tick(60)
-    finally:
-        pygame.quit()
+pygame.display.set_caption("Whack-A-Mole!")
+screen = pygame.display.set_mode((640, 512))
+mole_image = pygame.image.load("mole.png")
+mole_rect = mole_image.get_rect(topleft=(3,3))
 
+running = True
 
-if __name__ == "__main__":
-    main()
+def new_board():
+    screen.fill((0, 150, 255))
+    screen.blit(mole_image, mole_rect)
+    for i in range(0, 640, 32):
+        pygame.draw.line(screen, (0, 0, 0), (i, 0), (i, 512))
+    for j in range(0, 512, 32):
+        pygame.draw.line(screen, (0, 0, 0), (0, j), (640, j))
+
+def move_mole():
+    mole_rect.topleft = random.randint(0, 19) * 32, random.randint(0, 15) * 32
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if mole_rect.collidepoint(event.pos):
+                move_mole()
+    new_board()
+    pygame.display.flip()
+
+pygame.quit()
